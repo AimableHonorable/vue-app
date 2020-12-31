6e0471386ffdd88fdbb5a873e7cbe06e9654b8b2
AimableHonorable <aimatwiri@gmail.com>
2020-12-31 11:14:46 +0200

    first vue application

---
+++ src/components/Posts.vue
@@ -0,0 +1,40 @@
+<template id="">
+  <div class="container">
+    <table class="table table-dark my-4">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Title</th>
+      <th scope="col">Body</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="post in posts" :key="post.id">
+      <th scope="row">{{ post.id }}</th>
+      <td>{{ post.title }}</td>
+      <td>{{ post.body }}</td>
+    </tr>
+  </tbody>
+</table>
+  </div>
+</template>
+<script type="text/javascript">
+  import axios from 'axios';
+  export default{
+    name: 'PostsExample',
+    data(){
+      return{
+        posts: []
+      }
+    },
+    created(){
+      axios.get('https://jsonplaceholder.typicode.com/posts').then(posts=>{
+        this.posts = posts.data
+      })
+    }
+
+  }
+</script>
+<style media="screen">
+
+</style>
